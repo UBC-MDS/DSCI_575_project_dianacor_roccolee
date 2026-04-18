c239bf89be0240bcce0723bcfe10cc5398466d3c
Rocco Lee <roccolee18@gmail.com>
2026-04-17 22:47:07 -0700

    Add hybrid RAG pipeline to app_utils

--- app/app_utils.py
+++ app/app_utils.py
@@ -16,6 +16,17 @@ from langchain_core.documents import Document
 from langchain_community.retrievers import BM25Retriever
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+import pyarrow.parquet as pq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from transformers import pipeline
+from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_classic.retrievers import EnsembleRetriever
+
 nltk.download("stopwords", quiet=True)
 STOP_WORDS = set(stopwords.words("english"))
 
@@ -118,8 +129,68 @@ def bm25_search(query, bm25, docs, k = 5):
         for i in top_k
     ]
 
+############################## Query using Hybrid RAG search ##############################
 
+def build_hybrid_retriever():
+    embeddings = HuggingFaceEmbeddings(
+        model_name= "sentence-transformers/all-MiniLM-L6-v2")
 
+    vectorstore = FAISS.load_local(
+            "data/processed/langchain_semantic_index", 
+            embeddings, allow_dangerous_deserialization=True
+        )
+    
+    docs = pd.read_parquet("data/processed/product_documents.parquet")
+    documents = [
+        Document(
+            page_content=row["document"],
+            metadata={"product_title": row["product_title"], "parent_asin": row["parent_asin"], "average_rating": row["average_rating"]}
+        )
+        for _, row in docs.iterrows()
+    ]
+
+    bm25_retriever = BM25Retriever.from_documents(documents)
+    bm25_retriever.k = 5
+
+    semantic_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5}) # Fetch 5 most similar documents
+
+    hybrid_retriever = EnsembleRetriever(
+        retrievers=[bm25_retriever, semantic_retriever],
+        weights=[0.4, 0.6]  # Example: asigning 40% importance to BM25, 60% to Semantic Search
+    )
+    return hybrid_retriever
+
+def run_hybrid_chain(query):
+    hybrid_retriever = build_hybrid_retriever()
+
+    docs = hybrid_retriever.invoke(query)
+    context = build_context(docs)
+
+    generator = pipeline(
+                    "text-generation",
+                    model="Qwen/Qwen2.5-1.5B",
+                    max_new_tokens=128,
+                    do_sample=True,
+                )
+    llm = HuggingFacePipeline(pipeline=generator)
+    text_prompt = build_prompt(system_prompt, query, context)
+    full_prompt = ChatPromptTemplate.from_template(text_prompt)
+
+    hybrid_rag_chain = (
+            {
+                "context": hybrid_retriever |  RunnableLambda(build_context),
+                "query": RunnablePassthrough()
+            }
+            | full_prompt
+            | llm
+            | StrOutputParser()
+        )
+    
+    
+    response = hybrid_rag_chain.invoke(query)
+    response_cut = response.split("Assistant:", 1)[-1].strip()
+
+    return response_cut
 
 ############################## Langchain Utils Functions ##############################
 def load_documents(parquet_path = "data/processed/product_documents.parquet", 
@@ -212,9 +283,9 @@ def return_top_results(top_k):
 ############################## RAG pipeline Functions ##############################
 
 def build_context(docs):
-        return "\n\n".join(
-            f"Product ASIN: {doc.metadata.get('parent_asin', 'N/A')}\n"
-            f"Title: {doc.metadata.get('product_title', '')}\n"
-            # f"Rating: {doc.metadata['rating']}/5]\n" # Need to add back as part of web app requirements
-            for doc in docs
-        )
+    return "\n\n".join(
+        f"Product ASIN: {doc.metadata.get('parent_asin', 'N/A')}\n"
+        f"Title: {doc.metadata.get('product_title', '')}\n"
+        f"Average Rating: {doc.metadata.get('average_rating', '')}\n"
+        for doc in docs
+    )
